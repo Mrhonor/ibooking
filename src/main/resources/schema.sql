44872292fa9dbd33500a826eb7fb2972a28fe521
l00378932 <lichunhua5@huawei.com>
2022-03-08 17:46:03 +0800

    Create a StudyRoom root URL to prepare for front-end development

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,3 +8,30 @@ create table tbl_student
     primary key (id),
     unique (stuNum)
 );
+
+drop table if exists tbl_studyroom;
+create table tbl_study_room
+(
+    id           int         not null auto_increment,
+    buildingNum  varchar(16) not null,
+    classRoomNum varchar(16) not null,
+    primary key (id)
+);
+
+drop table if exists tbl_seat;
+create table tbl_seat
+(
+    id          int not null auto_increment,
+    studyRoomId int not null,
+    primary key (id, studyRoomId)
+);
+
+drop table if exists tbl_booking_xxx;
+create table tbl_booking_xxx
+(
+    id            int not null auto_increment,
+    seatId        int not null,
+    bookingPeriod int not null,
+    stuId         int not null,
+    primary key (id)
+);
