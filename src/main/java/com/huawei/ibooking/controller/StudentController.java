428d76a8c3c5acd2be347a23a12d30db08dd4b6f
l00378932 <lichunhua5@huawei.com>
2022-03-08 15:20:04 +0800

    Add Test Case shoule_be_success_when_query_all_students

---
+++ src/main/java/com/huawei/ibooking/controller/StudentController.java
@@ -0,0 +1,4 @@
+package com.huawei.ibooking.controller;
+
+public class StudentController {
+}
---
+++ src/test/java/com/huawei/ibooking/controller/StudentControllerTest.java
@@ -0,0 +1,59 @@
+package com.huawei.ibooking.controller;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.huawei.ibooking.BookingApplication;
+import com.huawei.ibooking.model.StudentDO;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BookingApplication.class)
+@WebAppConfiguration
+public class StudentControllerTest {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void shoule_be_success_when_query_all_students() throws Exception {
+        final MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+                        .get("/student")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andReturn();
+
+        final List<StudentDO> students = new ObjectMapper().readValue(
+                result.getResponse().getContentAsString(), new TypeReference<List<StudentDO>>() {
+                });
+
+        Assert.assertEquals(students.size(), 5);
+    }
+}
