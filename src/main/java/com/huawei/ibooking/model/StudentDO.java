9f77e0dfc49e900136a6436dd35d6a4705c230b7
Mrhonor <619500598@qq.com>
2023-03-27 14:52:26 +0800

    add github actions for CI

--- src/main/java/com/huawei/ibooking/model/StudentDO.java
+++ src/main/java/com/huawei/ibooking/model/StudentDO.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+// 1. @NoArgsConstructor - this annotation is to generate a constructor without parameters
+// 2. @Getter and @Setter - these two annotations are to generate getter and setter methods
 @NoArgsConstructor
 @Getter
 @Setter
