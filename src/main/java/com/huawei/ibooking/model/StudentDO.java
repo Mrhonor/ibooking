0c706accfcce814a9d3626838a13d70c721a60ed
Mrhonor <22210240239@m.fudan.edu.cn>
2023-05-28 19:35:43 +0800

    temp save

--- src/main/java/com/huawei/ibooking/model/StudentDO.java
+++ src/main/java/com/huawei/ibooking/model/StudentDO.java
@@ -11,8 +11,16 @@ import lombok.Setter;
 @Getter
 @Setter
 public class StudentDO {
-    private int id;
+    //学号
     private String stuNum;
+    //姓名
     private String name;
+    //登录密码
     private String password;
+	//是否为管理员
+	private String isAdmin;
+	//学生邮件
+    private String email;
+    //预约记录 用方法实现 不作为属性存在
+    //private List<ReservationDo> ReservationList ;
 }
